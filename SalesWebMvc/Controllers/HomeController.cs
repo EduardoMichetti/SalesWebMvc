3a20c15d587180b9b0af4acd59d8867b7cf8e6fb
Eduardo Michetti <94409995+EduardoMichetti@users.noreply.github.com>
2024-03-19 20:02:36 -0300

    Primeiro controller e teste Razor pages

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Web Vendas MVC App do curso de c#.";
+            ViewData["Professor"] = "Nelio Alves";
 
             return View();
         }
